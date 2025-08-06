0a6ee4556097596b3a1d19d486ed640aec0a21cc
Courage Ngwenya <ngwenyacoura@gmail.com>
2025-08-06 12:33:07 +0200

    Updated HTS and SI - final

--- hours_to_seconds.py
+++ hours_to_seconds.py
@@ -1,6 +1,6 @@
-hours = 9
+hours = 2
 seconds = (hours*60)*60
 variables = seconds
-print(f"{hours} is {seconds}.")
+print(f"{hours} hour(s) is {seconds} seconds.")
 
 
--- simple_interest.py
+++ simple_interest.py
@@ -2,7 +2,7 @@
 
 principal = 1000
 rate = 5
-time = 2
+time = 3
 
 interest = (principal * rate * time) / 100
-print(interest)
+print(f"The simple interest is: {interest}")
