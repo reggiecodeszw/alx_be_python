7996a2a6ee59d6faa913bef757a500c9f663574d
Courage Ngwenya <ngwenyacoura@gmail.com>
2025-08-06 11:39:39 +0200

    Removed 2nd pythonn_introduction folder

---
+++ future_age_calculator.py
@@ -0,0 +1,5 @@
+C_Age = int(input("How old are you?"))
+years = 27
+Age = C_Age + years
+
+print(f"In 2050, you will be {Age} years old.")
---
+++ hours_to_seconds.py
@@ -0,0 +1,6 @@
+hours = 9
+seconds = (hours*60)*60
+variables = seconds
+print(f"{hours} is {seconds}.")
+
+
